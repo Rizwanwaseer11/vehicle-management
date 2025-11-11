13087b485ea22f9ef2b03fbb8c23ab4220c68cc3
Rizwan Ahmed Waseer <rizwanwaseer98@gmail.com>
2025-11-11 17:06:47 +0500

    client project

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
+
+exports.register = async (req, res) => {
+  const { name, email, password, phone, role } = req.body;
+  const exists = await User.findOne({ email });
+  if (exists) return res.status(400).json({ message: 'Email already exists' });
+
+  const user = await User.create({ name, email, password, phone, role });
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id)
+  });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+  const match = await user.matchPassword(password);
+  if (!match) return res.status(400).json({ message: 'Invalid credentials' });
+
+  res.json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id)
+  });
+};
---
+++ readme.txt
@@ -0,0 +1,8 @@
+
+  "name": "school-transport-backend",
+  "version": "1.0.0",
+  "main": "cluster/master.js",
+  "scripts": {
+    "start": "node cluster/master.js",
+    "dev": "nodemon cluster/master.js"
+  },
