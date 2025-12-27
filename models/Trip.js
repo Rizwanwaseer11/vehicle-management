7a42ffc5a1be1542d0deb974eb79116b283fc5ed
Rizwan Ahmed Waseer <rizwanwaseer98@gmail.com>
2025-12-27 11:13:37 +0500

    update trip and stop schema

--- models/Trip.js
+++ models/Trip.js
@@ -1,22 +1,47 @@
 const { required } = require('joi');
 const mongoose = require('mongoose');
 
+// const stopSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   latitude: { type: Number, required: true },
+//   longitude: { type: Number, required: true },
+//   passengers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+// });
+
 const stopSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+   name: { type: String, required: true },
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true },
-  passengers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  order: {tyep : Number, required : true},
+
+  passengers: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    status: {
+      type: String,
+      enum: ['WAITING', 'BOARDED', 'MISSED'],
+      default: 'WAITING'
+    }
+  }]
 });
 
 const tripSchema = new mongoose.Schema({
+   routeName: { type: String, required: true },
   driver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
    bus: { type: mongoose.Schema.Types.ObjectId, ref: 'Bus', required: true },
-  routeName: { type: String, required: true },
+ 
   stops: [stopSchema],
   isActive: { type: Boolean, default: true },
+  status: {
+    type: String,
+    enum: ['SCHEDULED', 'ONGOING', 'COMPLETED', 'CANCELLED'],
+    default: 'SCHEDULED'
+  },
+    currentStopIndex: { type: Number, default: 0 },
   startTime: { type: Date },
   endTime: { type: Date },
   totalKm: { type: Number, default: 0 ,required: true}
 }, { timestamps: true });
+
+
 
 module.exports = mongoose.model('Trip', tripSchema);
